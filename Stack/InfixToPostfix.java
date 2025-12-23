840a74865cc8a5a306793685d4990bf5202bd412
Shaghayegh Ghasemi <shaghayegh@Shaghayeghs-MacBook-Air.local>
2025-12-23 10:07:13 -0500

    FIX - right associative operator check - Infix expression to Postfix expression using stack

--- Stack/InfixToPostfix.java
+++ Stack/InfixToPostfix.java
@@ -1,9 +1,8 @@
-import java.util.Arrays;
 import java.util.Stack;
 
 public class InfixToPostfix {
 
-    private int checkPriority(char o){
+    static int checkPriority(char o){
         if (o == '^') return 3;
         else if (o == '/' || o == '*' ) {
             return 2;
@@ -13,6 +12,10 @@ public class InfixToPostfix {
         return -1;
     }
 
+    static boolean isRightAssociative(char o){
+        return o == '^';
+    }
+
     public String infixToPostfix(String s){
         StringBuilder postfix = new StringBuilder();
         Stack<Character> stack = new Stack<>();
@@ -20,18 +23,20 @@ public class InfixToPostfix {
         for(int i = 0; i < s.length(); i++){
             char c = s.charAt(i);
 
-            // when we see an operand / add it to result
+            // when we see an operand - add it to result
             if(Character.isLetterOrDigit(c)){
                 postfix.append(c);
-            } else if (c == '(') {
+            } else if (c == '(') { // push opening parentheses to stack
                 stack.push(c);
-            } else if (c == ')') {
-                while (stack.peek() != '(' && !stack.isEmpty()){
+            } else if (c == ')') { // pop until opening parentheses and add it to result
+                while (!stack.isEmpty() && stack.peek() != '('){
                     postfix.append(stack.pop());
                 }
-                stack.pop();
-            } else {
-                while(!stack.isEmpty() && checkPriority(c) < checkPriority(stack.peek())){
+                if (!stack.isEmpty()) stack.pop();
+            } else { // other operators
+                while (!stack.isEmpty() && stack.peek() != '(' &&
+                        (checkPriority(c) < checkPriority(stack.peek()) ||
+                                (checkPriority(c) == checkPriority(stack.peek()) && !isRightAssociative(c)))) {
                     postfix.append(stack.pop());
                 }
                 stack.push(c);
@@ -49,15 +54,9 @@ public class InfixToPostfix {
     public static void main(String[] args) {
         InfixToPostfix solution = new InfixToPostfix();
 
-        String infix1 = "a+b*c";
-        String postfix1 = solution.infixToPostfix(infix1);
-
-        System.out.println("Result of converting infix expression: " + infix1 + " to postfix expression is: " + postfix1);
-
-        String infix2 = "(a+b)*c";
-        String postfix2 = solution.infixToPostfix(infix2);
-
-        System.out.println("Result of converting infix expression: " + infix2 + " to postfix expression is: " + postfix2);
-
+        String[] tests = {"a+b*c", "(a+b)*c", "a-b-c", "a^b^c", "(a+b)*c"};
+        for (String infix : tests) {
+            System.out.println("Infix: " + infix + " -> Postfix: " + solution.infixToPostfix(infix));
+        }
     }
 }
